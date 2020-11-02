540869d3d9bd5aa17c9257ff3b7203e0659645d2
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-11-02 07:29:25 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,10 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-     erb :account
+     @user= User.find_by(username: params[:username], password: params[:password])
+    if @user
+      session[:user_id] = @user.id
+      redirect '/account'
     else
       erb :error
     end
